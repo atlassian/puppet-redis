6e5dfdce9f1fe9929ceae2070171aff1cf9fdbb2
Danil Koudlo <dkoudlo@atlassian.com>
2019-08-22 16:35:48 -0700

    add template for the limit.conf of systemd's process

--- manifests/service.pp
+++ manifests/service.pp
@@ -11,6 +11,15 @@ class redis::service {
       hasstatus  => $::redis::service_hasstatus,
       provider   => $::redis::service_provider,
     }
+
+    # If maxclients more then redis default 
+    # Set service LimitNOFILE limit to the new value
+    # systemd has to be explicitly defined instead of puppet figuring it out
+    if $::redis::params::maxclients > 10000 and $::redis::service_provider == 'systemd' {
+      file { '/etc/systemd/system/' + $::redis::service_name + '.service.d/limit.conf':
+        notify  => Service[$::redis::service_name],  # this sets up the relationship
+        content => template('systemd.limit.conf.erb'),
+      }
+    }
   }
 }
-
