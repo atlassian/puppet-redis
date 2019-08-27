32ca698f41907e90e5d5be0892161d57bb102644
Danil Koudlo <dkoudlo@atlassian.com>
2019-08-27 14:55:30 -0700

    ensure limit set

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,6 +3,23 @@
 # This class manages the Redis daemon.
 #
 class redis::service {
+
+  exec {'systemd daemon-reload':
+    refreshonly => true,
+    command     => 'systemctl daemon-reload',
+  }
+
+  # If maxclients more then OS default default 
+  # Set service LimitNOFILE limit to the new value
+  # systemd has to be explicitly defined instead of puppet figuring it out
+  if $::redis::params::maxclients > 4096 {
+    file { '/etc/systemd/system/' + $::redis::service_name + '.service.d/limit.conf':
+      ensure  => file,
+      notify  => Exec['systemd daemon-reload'],
+      content => template('systemd.limit.conf.erb'),
+    }
+  }
+
   if $::redis::service_manage {
     service { $::redis::service_name:
       ensure     => $::redis::service_ensure,
@@ -10,16 +27,6 @@ class redis::service {
       hasrestart => $::redis::service_hasrestart,
       hasstatus  => $::redis::service_hasstatus,
       provider   => $::redis::service_provider,
-    }
-
-    # If maxclients more then redis default 
-    # Set service LimitNOFILE limit to the new value
-    # systemd has to be explicitly defined instead of puppet figuring it out
-    if $::redis::params::maxclients > 10000 and $::redis::service_provider == 'systemd' {
-      file { '/etc/systemd/system/' + $::redis::service_name + '.service.d/limit.conf':
-        notify  => Service[$::redis::service_name],  # this sets up the relationship
-        content => template('systemd.limit.conf.erb'),
-      }
     }
   }
 }
