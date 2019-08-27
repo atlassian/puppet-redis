6501343c19706c7ed54840217801e60a5d9b573b
Danil Koudlo <dkoudlo@atlassian.com>
2019-08-27 16:01:42 -0700

    apply limits only when puppet service is managed

--- manifests/service.pp
+++ manifests/service.pp
@@ -4,15 +4,14 @@
 #
 class redis::service {
   if $::redis::service_manage {
-
-    exec {'systemd daemon-reload':
-      refreshonly => true,
-      command     => 'systemctl daemon-reload',
-    }
-
     # If maxclients more then OS default default 
     # Set service LimitNOFILE limit to the new value
     if $::redis::params::maxclients > 10000 {
+      exec {'systemd daemon-reload':
+        refreshonly => true,
+        command     => 'systemctl daemon-reload',
+      }
+
       file { '/etc/systemd/system/' + $::redis::service_name + '.service.d/limit.conf':
         ensure  => file,
         notify  => Exec['systemd daemon-reload'],
