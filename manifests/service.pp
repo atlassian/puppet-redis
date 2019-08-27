7adaab200db46416f0bdb2205a00cd87e075dcd0
Danil Koudlo <dkoudlo@atlassian.com>
2019-08-27 15:59:37 -0700

    add limit template and update comments

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,24 +3,23 @@
 # This class manages the Redis daemon.
 #
 class redis::service {
-
-  exec {'systemd daemon-reload':
-    refreshonly => true,
-    command     => 'systemctl daemon-reload',
-  }
-
-  # If maxclients more then OS default default 
-  # Set service LimitNOFILE limit to the new value
-  # systemd has to be explicitly defined instead of puppet figuring it out
-  if $::redis::params::maxclients > 4096 {
-    file { '/etc/systemd/system/' + $::redis::service_name + '.service.d/limit.conf':
-      ensure  => file,
-      notify  => Exec['systemd daemon-reload'],
-      content => template('systemd.limit.conf.erb'),
-    }
-  }
-
   if $::redis::service_manage {
+
+    exec {'systemd daemon-reload':
+      refreshonly => true,
+      command     => 'systemctl daemon-reload',
+    }
+
+    # If maxclients more then OS default default 
+    # Set service LimitNOFILE limit to the new value
+    if $::redis::params::maxclients > 10000 {
+      file { '/etc/systemd/system/' + $::redis::service_name + '.service.d/limit.conf':
+        ensure  => file,
+        notify  => Exec['systemd daemon-reload'],
+        content => template('systemd.limit.conf.erb'),
+      }
+    }
+
     service { $::redis::service_name:
       ensure     => $::redis::service_ensure,
       enable     => $::redis::service_enable,
